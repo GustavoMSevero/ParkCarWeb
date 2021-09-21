57b8ae48810f606b1975c52069f13c05e3f54ad8
GustavoMSevero <gustavo.msevero@gmail.com>
2021-09-21 12:01:15 -0300

    Adjusted PHPMailer importing.

--- api/parking/parking.php
+++ api/parking/parking.php
@@ -11,9 +11,9 @@ $pdo = conectar();
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
