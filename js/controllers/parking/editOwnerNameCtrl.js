d0ac775ac0ab569720cd0d9c9bf4fff3b3c90d94
GustavoMSevero <gustavo.msevero@gmail.com>
2021-10-06 12:12:28 -0300

    Created editOwnerName js.

---
+++ js/controllers/parking/editOwnerNameCtrl.js
@@ -0,0 +1,49 @@
+app.controller("editOwnerNameCtrl", ['$scope', '$http', '$location', function ($scope, $http, $location) {
+
+    $scope.typeUser = localStorage.getItem('parkcar_typeUser'); 
+    if ($scope.typeUser == 'parking') {
+        $scope.parking = $scope.typeUser;
+        $scope.id = localStorage.getItem('parkcar_id');
+        $scope.name = localStorage.getItem('parkcar_name');
+        $scope.parking = localStorage.getItem('parkcar_typeUser');
+        console.log($scope.id +' '+ $scope.name +' '+ $scope.typeUser +' '+ $scope.parking);
+
+    } else {
+        $scope.admin = $scope.typeUser;
+        $scope.id = localStorage.getItem('parkcar_id');
+        $scope.name = localStorage.getItem('parkcar_name');
+        $scope.typeUser =  $scope.admin = localStorage.getItem('parkcar_typeUser');
+        // console.log($scope.id +' '+ $scope.name +' '+ $scope.typeUser +' '+ $scope.admin);
+    }
+    
+
+	if(location.hostname == 'localhost'){
+		var urlPrefixAdmin = 'http://localhost:8888/Projects/Web/ParkCarWeb/api/admin/adminParking.php';
+        var urlOptionPrefixAdmin = 'http://localhost:8888/Projects/Web/ParkCarWeb/api/admin/adminParking.php?option=';
+	} else {
+		var urlPrefixAdmin = 'api/admin/adminParking.php';
+        var urlOptionPrefixAdmin = 'api/admin/adminParking.php?option=';
+	}
+
+    var getOwnerName = function() {
+        var option = 'get owner name';
+        $http.get(urlOptionPrefixAdmin + option + '&id=' + $scope.id).success(function(response) {
+            // console.log(response)
+            // $scope.newOwner = response.name;
+        })
+    }
+    getOwnerName();
+
+    $scope.updateOwnerName = function(owner) {
+        owner.option = 'update owner name';
+        owner.id = $scope.id;
+
+        $http.post(urlPrefixAdmin, owner).success(function(data) {
+            // console.log(data.name)
+            $scope.name = data.name;
+            localStorage.setItem('parkcar_name', $scope.name);
+            $location.path('/ownerArea');
+        })
+    }
+
+}]);
