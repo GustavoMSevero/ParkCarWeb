18f93ba203e0406e348717d628085c9c106adccc
GustavoMSevero <gustavo.msevero@gmail.com>
2021-10-27 11:18:10 -0300

    Imported files and registering credits to user account.

--- api/stripe.php
+++ api/stripe.php
@@ -1,10 +1,15 @@
 <?php
 include_once("stripe-php-7.100.0/init.php");
 include_once("config/config.php");
+include_once("functions/jwt.php");
+include_once("con.php");
+
+$pdo = conectar();
 
 $stripe = new \Stripe\StripeClient($stripClient);
 $apiKey = \Stripe\Stripe::setApiKey($setApiKey);
 $price = $_GET['price'] ?? 20;
+$idClient = verifyJWT();
 
 $customer = \Stripe\Customer::create();
 
@@ -23,6 +28,13 @@ $response = array(
     'ephemeralKey' => $ephemeralKey->secret,
     'customer' => $customer->id
 );
+
+$buyCredits=$pdo->prepare("INSERT INTO credits (idCredits, idClient, customerStripe, creditValue) VALUES(?,?,?,?)");
+$buyCredits->bindValue(1, NULL);
+$buyCredits->bindValue(2, $idClient);
+$buyCredits->bindValue(3, $response['customer']);
+$buyCredits->bindValue(4, $price);
+$buyCredits->execute();
 
 
 echo json_encode($response);
