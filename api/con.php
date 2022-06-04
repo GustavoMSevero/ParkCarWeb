1e32e01d0cc8984eb295bdc944c50ade79a47018
GustavoMSevero <gustavo.msevero@gmail.com>
2022-06-04 20:37:30 -0300

    Deleted commented lines.

--- api/con.php
+++ api/con.php
@@ -11,11 +11,6 @@ error_reporting(E_ALL);
 			$user = "root";
 			$password = "root";
 
-			// $first = "mysql:host=mysql.parkcar.app.br;dbname=";
-			// $dbname = "parkcar";
-			// $user = "parkcar";
-			// $password = "ParkCar2021";
-
 			$opcoes = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 			$pdo = new PDO($first.$dbname, $user, $password, $opcoes);	
 		} catch (Exception $e) {
