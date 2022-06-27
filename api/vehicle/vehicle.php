94cd662221177ba90c91baa544e44a176fd3b954
GustavoMSevero <gustavo.msevero@gmail.com>
2022-06-27 18:16:36 -0300

    Getting data and save for ticket entry.

--- api/vehicle/vehicle.php
+++ api/vehicle/vehicle.php
@@ -399,7 +399,7 @@ switch ($option) {
 
             file_get_contents('http://ws.parkcar.app.br/vacancies/'.$idParking.'/'.$vaccantNumber);
 
-            // GET ENTRY TICKET INFO
+            // GET ENTRY TICKET INFORMATIONS
             $getParkingData=$pdo->prepare("SELECT parkingName, address FROM parking WHERE idParking=:idParking");
             $getParkingData->bindValue(":idParking", $idParking);
             $getParkingData->execute();
@@ -409,31 +409,24 @@ switch ($option) {
                 $parkingAddress = $line['address'];
             }
 
-            if ($cnpj == '') {
-                $cnpj = null;
-            }
-
-            if ($parkingPhone == '') {
-                $parkingPhone = null;
-            }
-
             $licensePlate;
             $entranceP = explode(" ", $entrance);
-            $entrayDate = $entranceP[0];
-            $entrayTime = $entranceP[1];
+            $entryDate = $entranceP[0];
+            $entryTime = $entranceP[1];
+            $status = 1;
 
             // SAVE ENTRY TICKET INFO
-            $insertEntrayTicketInfo=$pdo->prepare("INSERT INTO entry_ticket (id_entry_ticket, entrance, parkingName, parkingAddress, CNPJ, 
-                                                parkingPhone, licensePlate, entryDate, entryTime) VALUES(?,?,?,?,?,?,?,?,?)");
-            $insertEntrayTicketInfo->bindValue(0, NULL);
-            $insertEntrayTicketInfo->bindValue(1, $entrance);
+            $insertEntrayTicketInfo=$pdo->prepare("INSERT INTO entry_ticket (id_entry_ticket, parkingName, parkingAddress, CNPJ, parkingPhone, licensePlate, 
+                                                entryDate, entryTime, statusTicket) VALUES(?,?,?,?,?,?,?,?,?)");
+            $insertEntrayTicketInfo->bindValue(1, NULL);
             $insertEntrayTicketInfo->bindValue(2, $parkingName);
             $insertEntrayTicketInfo->bindValue(3, $parkingAddress);
             $insertEntrayTicketInfo->bindValue(4, $cnpj);
             $insertEntrayTicketInfo->bindValue(5, $parkingPhone);
             $insertEntrayTicketInfo->bindValue(6, $licensePlate);
-            $insertEntrayTicketInfo->bindValue(7, $entrayDate);
-            $insertEntrayTicketInfo->bindValue(8, $entrayTime);
+            $insertEntrayTicketInfo->bindValue(7, $entryDate);
+            $insertEntrayTicketInfo->bindValue(8, $entryTime);
+            $insertEntrayTicketInfo->bindValue(9, $status);
             $insertEntrayTicketInfo->execute();
             
             
@@ -509,7 +502,77 @@ switch ($option) {
             $updateVaccantsNumber->execute();
 
             file_get_contents('http://ws.parkcar.app.br/vacancies/'.$idParking.'/'.$vaccantNumber);
-  
+
+            $statusForSearch = 1;
+            // SEARCH VEHICLE BY LICENSEPLATE AND STATUS TO GET TICKET INFORMATIONS
+            $getVehicleToExitTicket=$pdo->prepare("SELECT * FROM entry_ticket WHERE licensePlate=:licensePlate 
+                                                AND statusTicket=:status");
+            $getVehicleToExitTicket->bindValue(":licensePlate", $licensePlate);
+            $getVehicleToExitTicket->bindValue(":status", $statusForSearch);
+            $getVehicleToExitTicket->execute();
+
+            // DATE FROM ENTRY TICKET
+            while ($line=$getVehicleToExitTicket->fetch(PDO::FETCH_ASSOC)) {
+                $parkingName = $line['parkingName'];
+                $parkingAddress = $line['parkingAddress'];
+                $cnpj = $line['CNPJ'];
+                $parkingPhone = $line['parkingPhone'];
+                $entryDate = $line['entryDate'];
+                $entryTime = $line['entryTime'];
+            }
+
+             // CALCULATION OF STAY
+             $eD = $entryDate;
+             $eT = $entryTime;
+             $entryDateAndTime = $entryDate." ".$entryTime;
+ 
+             $departureTimeP = explode(" ", $departureTime);
+             $exitDate = $departureTimeP[0];
+             $exitTime = $departureTimeP[1];
+ 
+             $date1= date_create($entryDateAndTime);
+             $date2= date_create($departureTime);
+             $diff=date_diff($date1,$date2);
+             $diff = $diff->format("%H:%I:%S");
+ 
+             $paymentType = "DEBITO";
+ 
+             $status = 0;
+
+            // SAVE DEPARTURE DATA TICKET
+            $insertDepartureData=$pdo->prepare("INSERT INTO exit_ticket (id_exit_ticket, parkingName, parkingAddress, CNPJ, parkingPhone, licensePlate, entryDate, 
+                    entryTime, exitDate, exitTime, stayOfTime, paymentType, statusTicket) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            $insertDepartureData->bindValue(1, NULL);
+            $insertDepartureData->bindValue(2, $parkingName);
+            $insertDepartureData->bindValue(3, $parkingAddress);
+            $insertDepartureData->bindValue(4, $cnpj);
+            $insertDepartureData->bindValue(5, $parkingPhone);
+            $insertDepartureData->bindValue(6, $licensePlate);
+            $insertDepartureData->bindValue(7, $entryDate);
+            $insertDepartureData->bindValue(8, $entryTime);
+            $insertDepartureData->bindValue(9, $exitDate);
+            $insertDepartureData->bindValue(10, $exitTime);
+            $insertDepartureData->bindValue(11, $diff);
+            $insertDepartureData->bindValue(12, $paymentType);
+            $insertDepartureData->bindValue(13, $status);
+            $insertDepartureData->execute();
+
+            $return = array(
+                'parkingName' => $parkingName,
+                'parkingAddress' => $parkingAddress,
+                'cnpj' => $cnpj,
+                'parkingPhone' => $parkingPhone,
+                'licensePlate' => $licensePlate,
+                'entryDate' => $entryDate,
+                'entryTime' => $entryTime,
+                'exitDate' => $exitDate,
+                'exitTime' => $exitTime,
+                'stayOfTime' => $stayOfTime,
+                'paymentType' => $paymentType,
+                'status' => $status
+            );
+
+            echo json_encode($return);
 
         } catch (Exception $e) {
             echo 'Caught exception: ',  $e->getMessage(), "\n";
