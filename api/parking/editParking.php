dd956d9314ffaaa3a9eabfb593eb9c121a7d589e
GustavoMSevero <gustavo.msevero@gmail.com>
2022-06-27 18:17:34 -0300

    Added CNPJ field in database.

--- api/parking/editParking.php
+++ api/parking/editParking.php
@@ -72,6 +72,7 @@ switch ($option) {
             while ($line=$getParkingToUpdate->fetch(PDO::FETCH_ASSOC)) {
 
                 $zipcode = $line['zipcode'];
+                $cnpj = $line['CNPJ'];
                 $parkingName = $line['parkingName'];
                 $address = $line['address'];
                 $addressNumber = $line['addressNumber'];
@@ -79,34 +80,17 @@ switch ($option) {
                 $city = $line['city'];
                 $state = $line['state'];
                 $vaccantNumber = $line['vaccantNumber'];
-                
-                // @$toleranceTime = $line['toleranceTime'];
-                // @$timeOne = $line['timeOne'];
-                // @$priceOne = $line['priceOne'];
-                // @$timeTwo = $line['timeTwo'];
-                // @$priceTwo = $line['priceTwo'];
-                // @$timeThree = $line['timeThree'];
-                // @$priceThree = $line['priceThree'];
-                // @$adicionalPrice = $line['adicionalPrice'];
 
                 $return = array(
                     'cep' => $zipcode,
+                    'cnpj' => $cnpj,
                     'newParkingName' => $parkingName,
                     'logradouro' => utf8_encode($address),
                     'newAddressNumber' => $addressNumber,
                     'bairro' => $neighborhood,
                     'localidade' => $city,
                     'uf' => $state,
-
                     'newVaccantNumber' => $vaccantNumber,
-                    // 'newToleranceTime' => $toleranceTime,
-                    // 'newTimeOne' => $timeOne,
-                    // 'newPriceOne' => $priceOne,
-                    // 'newTimeTwo' => $timeTwo,
-                    // 'newPriceTwo' => $priceTwo,
-                    // 'newTimeThree' => $timeThree,
-                    // 'newPriceThree' => $priceThree,
-                    // 'newAdicionalPrice' => $adicionalPrice
                 );
     
             }
@@ -121,7 +105,6 @@ switch ($option) {
         break;
 
     case 'update parking':
-        // print_r($data);
         $zipcode = $data->cep;
         $parkingName = $data->newParkingName;
         $address = $data->logradouro;
@@ -131,6 +114,7 @@ switch ($option) {
         $state = $data->uf;
         $newVaccantNumber = $data->newVaccantNumber;
         $idParking = $data->idParking;
+        $cnpj = $data->cnpj;
 
         try {
             
@@ -153,7 +137,8 @@ switch ($option) {
         try {
             $address = utf8_decode($address);
             $updateParking=$pdo->prepare("UPDATE parking 
-                                            SET parkingName=:parkingName, zipcode=:zipcode, address=:address, addressNumber=:addressNumber, neighborhood=:neighborhood, city=:city, state=:state, latitude=:latitude, longitude=:longitude
+                                            SET parkingName=:parkingName, zipcode=:zipcode, address=:address, addressNumber=:addressNumber, 
+                                            neighborhood=:neighborhood, city=:city, state=:state, latitude=:latitude, longitude=:longitude, cnpj=:cnpj
                                             WHERE idParking=:idParking");
             $updateParking->bindValue(":idParking", $idParking);
             $updateParking->bindValue(":parkingName", $parkingName);
@@ -165,55 +150,8 @@ switch ($option) {
             $updateParking->bindValue(":state", $state);
             $updateParking->bindValue(":latitude", $latitude);
             $updateParking->bindValue(":longitude", $longitude);
+            $updateParking->bindValue(":cnpj", $cnpj);
             $updateParking->execute();
-
-            // $toleranceTime = $data->newToleranceTime;
-            // $timeOne = $data->newTimeOne;
-            // $priceOne = $data->newPriceOne;
-            // $timeTwo = $data->newTimeTwo;
-            // $priceTwo = $data->newPriceTwo;
-            // $timeThree = $data->newTimeThree;
-            // $priceThree = $data->newPriceThree;
-            // $adicionalPrice = $data->newAdicionalPrice;
-            
-
-            // $toleranceTimeP = explode(':', $toleranceTime);
-            // $toleranceTime = $toleranceTimeP[0];
-
-            // $timeOneP = explode(':', $timeOne);
-            // $timeOne = $timeOneP[0];
-
-            // $timeTwoP = explode(':', $timeTwo);
-            // $timeTwo = $timeTwoP[0];
-
-            // $timeThreeP = explode(':', $timeThree);
-            // $timeThree = $timeThreeP[0];
-
-            // $priceOneP = explode(',', $priceOne);
-            // $priceOne = $priceOneP[0];
-
-            // $priceTwoP = explode(',', $priceTwo);
-            // $priceTwo = $priceTwoP[0];
-            
-            // $priceThreeP = explode(',', $priceThree);
-            // $priceThree = $priceThreeP[0];
-
-            // $adicionalPriceP = explode(',', $adicionalPrice);
-            // $adicionalPrice = $adicionalPriceP[0];
-
-            // $updateTimeAndPrices=$pdo->prepare("UPDATE parkingTimeAndPrices
-            //                                     SET toleranceTime=:toleranceTime, timeOne=:timeOne, priceOne=:priceOne, timeTwo=:timeTwo, priceTwo=:priceTwo, timeThree=:timeThree, priceThree=:priceThree, adicionalPrice=:adicionalPrice
-            //                                     WHERE idParking=:idParking");
-            // $updateTimeAndPrices->bindValue(":toleranceTime", $toleranceTime);
-            // $updateTimeAndPrices->bindValue(":timeOne", $timeOne);
-            // $updateTimeAndPrices->bindValue(":priceOne", $priceOne);
-            // $updateTimeAndPrices->bindValue(":timeTwo", $timeTwo);
-            // $updateTimeAndPrices->bindValue(":priceTwo", $priceTwo);
-            // $updateTimeAndPrices->bindValue(":timeThree", $timeThree);
-            // $updateTimeAndPrices->bindValue(":priceThree", $priceThree);
-            // $updateTimeAndPrices->bindValue(":adicionalPrice", $adicionalPrice);
-            // $updateTimeAndPrices->bindValue(":idParking", $idParking);
-            // $updateTimeAndPrices->execute();
 
             $status = 1;
             $msg = 'Dados atualizados com sucesso';
