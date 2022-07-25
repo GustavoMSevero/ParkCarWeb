96cdb989a732ea5c8c7c8610188394677041ed84
GustavoMSevero <gustavo.msevero@gmail.com>
2022-07-25 16:07:02 -0300

    Changed table name and added rows from exit_ticket table.

--- api/functions/generateEntryTicket.php
+++ api/functions/generateEntryTicket.php
@@ -28,11 +28,18 @@
             $entranceP = explode(" ", $entrance);
             $entryDate = $entranceP[0];
             $entryTime = $entranceP[1];
+
+            $exitDate = '0000-00-00';
+            $exitTime = '00:00:00';
+            $stayOfTime = '00:00:00';
+            $paymentType = '';
+            $amountPaid = '0.0';
+
             $status = 1;
 
             // SAVE ENTRY TICKET INFO
-            $insertEntrayTicketInfo=$pdo->prepare("INSERT INTO entry_ticket (id_entry_ticket, parkingName, parkingAddress, CNPJ, parkingPhone, licensePlate, 
-                                                entryDate, entryTime, statusTicket) VALUES(?,?,?,?,?,?,?,?,?)");
+            $insertEntrayTicketInfo=$pdo->prepare("INSERT INTO ticket (id_ticket, parkingName, parkingAddress, CNPJ, parkingPhone, licensePlate, 
+                                                entryDate, entryTime, exitDate, exitTime, stayOfTime, paymentType, amountPaid, statusTicket) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
             $insertEntrayTicketInfo->bindValue(1, NULL);
             $insertEntrayTicketInfo->bindValue(2, $parkingName);
             $insertEntrayTicketInfo->bindValue(3, $parkingAddress);
@@ -41,7 +48,12 @@
             $insertEntrayTicketInfo->bindValue(6, $licensePlate);
             $insertEntrayTicketInfo->bindValue(7, $entryDate);
             $insertEntrayTicketInfo->bindValue(8, $entryTime);
-            $insertEntrayTicketInfo->bindValue(9, $status);
+            $insertEntrayTicketInfo->bindValue(9, $exitDate);
+            $insertEntrayTicketInfo->bindValue(10, $exitTime);
+            $insertEntrayTicketInfo->bindValue(11, $stayOfTime);
+            $insertEntrayTicketInfo->bindValue(12, $paymentType);
+            $insertEntrayTicketInfo->bindValue(13, $amountPaid);
+            $insertEntrayTicketInfo->bindValue(14, $status);
             $insertEntrayTicketInfo->execute();
 
             // SAVE JUST ENTRY DATE AND ENTRY TIME TO BE LOCATED LATER TO DOWN TICKET 
