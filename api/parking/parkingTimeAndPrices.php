b652fc4eb36324c7640a2d27412704cbdbb0daa0
GustavoMSevero <gustavo.msevero@gmail.com>
2022-08-23 16:03:56 -0300

    Changed some name data and add toleranceDay, addPeriod, addValue and dailyPeriod.

--- api/parking/parkingTimeAndPrices.php
+++ api/parking/parkingTimeAndPrices.php
@@ -42,21 +42,26 @@ switch ($option) {
         $parkingTime_2 = $data->parkingTime_2;
         $parkingPrice_2 = $data->parkingPrice_2;
 
-        $additionalTime_1 = $data->additionalTime_1;
-        $additionalPrice_1 = $data->additionalPrice_1;
+        $parkingTime_3 = $data->parkingTime_3;
+        $parkingPrice_3 = $data->parkingPrice_3;
 
-        $additionalTime_2 = $data->additionalTime_2;
-        $additionalPrice_2 = $data->additionalPrice_2;
+        $parkingTime_4 = $data->parkingTime_4;
+        $parkingPrice_4 = $data->parkingPrice_4;
 
         $daily = $data->daily;
+
+        $toleranceDay = $data->toleranceDay;
+        $addPeriod = $data->addPeriod;
+        $addValue = $data->addValue;
+        $dailyPeriod = $data->dailyPeriod;
 
         $idParking = $data->idParking;
         $idParkingBranch = $data->idParkingBranch;
 
         $parkingPrice_1 = str_replace(",",".",$parkingPrice_1);
         $parkingPrice_2 = str_replace(",",".",$parkingPrice_2);
-        $additionalPrice_1 = str_replace(",",".",$additionalPrice_1);
-        $additionalPrice_2 = str_replace(",",".",$additionalPrice_2);
+        $parkingPrice_3 = str_replace(",",".",$parkingPrice_3);
+        $parkingPrice_4 = str_replace(",",".",$parkingPrice_4);
         $daily = str_replace(",",".",$daily);
 
         try {
@@ -68,19 +73,23 @@ switch ($option) {
 
             if ($qtd != 0) {
 
-                $updateTimeAndPrices=$pdo->prepare("UPDATE timeAndPrices SET tolerancePeriod=:tolerancePeriod, parkingTime_1=:parkingTime_1, parkingPrice_1=:parkingPrice_1,
-                                                    parkingTime_2=:parkingTime_2, parkingPrice_2=:parkingPrice_2, additionalTime_1=:additionalTime_1, additionalPrice_1=:additionalPrice_1,
-                                                    additionalTime_2=:additionalTime_2, additionalPrice_2=:additionalPrice_2, daily=:daily
+                $updateTimeAndPrices=$pdo->prepare("UPDATE timeAndPrices SET toleranceDay=:toleranceDay, tolerancePeriod=:tolerancePeriod, parkingTime_1=:parkingTime_1, parkingPrice_1=:parkingPrice_1,
+                                                    parkingTime_2=:parkingTime_2, parkingPrice_2=:parkingPrice_2, parkingTime_3=:parkingTime_3, parkingPrice_3=:parkingPrice_3,
+                                                    parkingTime_4=:parkingTime_4, parkingPrice_4=:parkingPrice_4, addPeriod=:addPeriod, addValue=:addValue, dailyPeriod=:dailyPeriod, daily=:daily
                                                     WHERE idParkingTimeAndPrices=:idParkingTimeAndPrices");
+                $updateTimeAndPrices->bindValue(":toleranceDay", $toleranceDay); 
                 $updateTimeAndPrices->bindValue(":tolerancePeriod", $tolerancePeriod); 
                 $updateTimeAndPrices->bindValue(":parkingTime_1", $parkingTime_1); 
                 $updateTimeAndPrices->bindValue(":parkingPrice_1", $parkingPrice_1); 
                 $updateTimeAndPrices->bindValue(":parkingTime_2", $parkingTime_2); 
                 $updateTimeAndPrices->bindValue(":parkingPrice_2", $parkingPrice_2); 
-                $updateTimeAndPrices->bindValue(":additionalTime_1", $additionalTime_1); 
-                $updateTimeAndPrices->bindValue(":additionalPrice_1", $additionalPrice_1); 
-                $updateTimeAndPrices->bindValue(":additionalTime_2", $additionalTime_2); 
-                $updateTimeAndPrices->bindValue(":additionalPrice_2", $additionalPrice_2); 
+                $updateTimeAndPrices->bindValue(":parkingTime_3", $parkingTime_3); 
+                $updateTimeAndPrices->bindValue(":parkingPrice_3", $parkingPrice_3); 
+                $updateTimeAndPrices->bindValue(":parkingTime_4", $parkingTime_4); 
+                $updateTimeAndPrices->bindValue(":parkingPrice_4", $parkingPrice_4); 
+                $updateTimeAndPrices->bindValue(":addPeriod", $addPeriod);
+                $updateTimeAndPrices->bindValue(":addValue", $addValue); 
+                $updateTimeAndPrices->bindValue(":dailyPeriod", $dailyPeriod); 
                 $updateTimeAndPrices->bindValue(":daily", $daily); 
                 $updateTimeAndPrices->bindValue(":idParkingTimeAndPrices", $idParkingTimeAndPrices); 
                 $updateTimeAndPrices->execute();
@@ -95,24 +104,28 @@ switch ($option) {
 
             } else {
 
-                $registerTimeAndPrices=$pdo->prepare("INSERT INTO timeAndPrices (idParkingTimeAndPrices, idParking, idSubparking, tolerancePeriod, parkingTime_1, parkingPrice_1,
-                                                    parkingTime_2, parkingPrice_2, additionalTime_1, additionalPrice_1, additionalTime_2, additionalPrice_2, daily) 
-                                                    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)");
+                $registerTimeAndPrices=$pdo->prepare("INSERT INTO timeAndPrices (idParkingTimeAndPrices, idParking, idSubparking, toleranceDay, tolerancePeriod, parkingTime_1, parkingPrice_1,
+                                                    parkingTime_2, parkingPrice_2, parkingTime_3, parkingPrice_3, parkingTime_4, parkingPrice_4, addPariod, addValue, dailyPeriod, daily) 
+                                                    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
                 $registerTimeAndPrices->bindValue(1, NULL);
                 $registerTimeAndPrices->bindValue(2, $idParking);
                 $registerTimeAndPrices->bindValue(3, $idParkingBranch);
 
-                $registerTimeAndPrices->bindValue(4, $tolerancePeriod);
-                $registerTimeAndPrices->bindValue(5, $parkingTime_1);
-                $registerTimeAndPrices->bindValue(6, $parkingPrice_1);
-                $registerTimeAndPrices->bindValue(7, $parkingTime_2);
-                $registerTimeAndPrices->bindValue(8, $parkingPrice_2);
+                $registerTimeAndPrices->bindValue(4, $toleranceDay);
+                $registerTimeAndPrices->bindValue(5, $tolerancePeriod);
+                $registerTimeAndPrices->bindValue(6, $parkingTime_1);
+                $registerTimeAndPrices->bindValue(7, $parkingPrice_1);
+                $registerTimeAndPrices->bindValue(8, $parkingTime_2);
+                $registerTimeAndPrices->bindValue(9, $parkingPrice_2);
 
-                $registerTimeAndPrices->bindValue(9, $additionalTime_1);
-                $registerTimeAndPrices->bindValue(10, $additionalPrice_1);
-                $registerTimeAndPrices->bindValue(11, $additionalTime_2);
-                $registerTimeAndPrices->bindValue(12, $additionalPrice_2);
-                $registerTimeAndPrices->bindValue(13, $daily);
+                $registerTimeAndPrices->bindValue(10, $parkingTime_3);
+                $registerTimeAndPrices->bindValue(11, $parkingPrice_3);
+                $registerTimeAndPrices->bindValue(12, $parkingTime_4);
+                $registerTimeAndPrices->bindValue(13, $parkingPrice_4);
+                $registerTimeAndPrices->bindValue(14, $addPeriod);
+                $registerTimeAndPrices->bindValue(15, $addValue);
+                $registerTimeAndPrices->bindValue(16, $dailyPeriod);
+                $registerTimeAndPrices->bindValue(17, $daily);
                 $registerTimeAndPrices->execute();
 
                 $msg = "Dados cadastrados com sucesso!";
@@ -151,6 +164,7 @@ switch ($option) {
                 $idParkingTimeAndPrices = $line['idParkingTimeAndPrices'];
                 $idSubparking = $line['idSubparking'];
 
+                $toleranceDay = $line['toleranceDay'];
                 $tolerancePeriod = $line['tolerancePeriod'];
                 $parkingTime_1 = $line['parkingTime_1'];
                 $parkingPrice_1 = $line['parkingPrice_1'];
@@ -160,29 +174,38 @@ switch ($option) {
                 $parkingPrice_2 = $line['parkingPrice_2'];
                 $parkingPrice_2 = str_replace(".",",",$parkingPrice_2);
 
-                $additionalTime_1 = $line['additionalTime_1'];
-                $additionalPrice_1 = $line['additionalPrice_1'];
-                $additionalPrice_1 = str_replace(".",",",$additionalPrice_1);
+                $parkingTime_3 = $line['parkingTime_3'];
+                $parkingPrice_3 = $line['parkingPrice_3'];
+                $parkingPrice_3 = str_replace(".",",",$parkingPrice_3);
 
-                $additionalTime_2 = $line['additionalTime_2'];
-                $additionalPrice_2 = $line['additionalPrice_2'];
-                $additionalPrice_2 = str_replace(".",",",$additionalPrice_2);
+                $parkingTime_4 = $line['parkingTime_4'];
+                $parkingPrice_4 = $line['parkingPrice_4'];
+                $parkingPrice_4 = str_replace(".",",",$parkingPrice_4);
 
+                $addPeriod = $line['addPeriod'];
+                $addValue = $line['addValue'];
+                $addValue = str_replace(".",",",$addValue);
+
+                $dailyPeriod = $line['dailyPeriod'];
                 $daily = $line['daily'];
                 $daily = str_replace(".",",",$daily);
                 
                 $return = array(
                     'idParkingTimeAndPrices' => $idParkingTimeAndPrices,
                     'idSubparking' => $idSubparking,
+                    'toleranceDay' => $toleranceDay,
                     'tolerancePeriod' => $tolerancePeriod,
                     'parkingTime_1' => $parkingTime_1,
                     'parkingPrice_1' => $parkingPrice_1,
                     'parkingTime_2' => $parkingTime_2,
                     'parkingPrice_2' => $parkingPrice_2,
-                    'additionalTime_1' => $additionalTime_1,
-                    'additionalPrice_1' => $additionalPrice_1,
-                    'additionalTime_2' => $additionalTime_2,
-                    'additionalPrice_2' => $additionalPrice_2,
+                    'parkingTime_3' => $parkingTime_3,
+                    'parkingPrice_3' => $parkingPrice_3,
+                    'parkingTime_4' => $parkingTime_4,
+                    'parkingPrice_4' => $parkingPrice_4,
+                    'addPeriod' => $addPeriod,
+                    'addValue' => $addValue,
+                    'dailyPeriod' => $dailyPeriod,
                     'daily' => $daily
                 );
 
