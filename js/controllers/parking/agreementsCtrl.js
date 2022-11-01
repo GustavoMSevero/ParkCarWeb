3c11d9c19ff122779a2d04eb1971076c89704ab8
GustavoMSevero <gustavo.msevero@gmail.com>
2022-11-01 18:01:54 -0300

    Created agreement controller file.

---
+++ js/controllers/parking/agreementsCtrl.js
@@ -0,0 +1,48 @@
+app.controller("agreementsCtrl", ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
+
+    $scope.typeUser = localStorage.getItem('parkcar_typeUser'); 
+    if ($scope.typeUser == 'parking') {
+        $scope.parking = $scope.typeUser;
+        $scope.id = localStorage.getItem('parkcar_id');
+        $scope.name = localStorage.getItem('parkcar_name');
+        $scope.parking = localStorage.getItem('parkcar_typeUser');
+        console.log($scope.id +' '+ $scope.name +' '+ $scope.typeUser +' '+ $scope.parking);
+
+    } else {
+        $scope.admin = $scope.typeUser;
+        $scope.id = localStorage.getItem('parkcar_id');
+        $scope.name = localStorage.getItem('parkcar_name');
+        $scope.typeUser =  $scope.admin = localStorage.getItem('parkcar_typeUser');
+        // console.log($scope.id +' '+ $scope.name +' '+ $scope.typeUser +' '+ $scope.admin);
+    }
+
+    if(location.hostname == 'localhost'){
+		var urlPrefixAgreement = 'http://localhost:8892/Projects/Web/ParkCarWeb/api/agreement/agreement.php';
+		var urlOptionPrefixAgreement = 'http://localhost:8892/Projects/Web/ParkCarWeb/api/agreement/agreement.php?option=';
+	} else {
+		var urlPrefixAgreement = 'api/agreements/agreement.php';
+        var urlOptionPrefixAgreement = 'api/agreements/agreement.php?option=';
+	}
+
+    $scope.agreement = {};
+    $scope.registerAgreement = function(agreement) {
+        agreement.option = 'save agreement';
+        agreement.idParking =  $scope.id;
+        $http.post(urlPrefixAgreement, agreement).success(function(data) {
+            getAgreements();
+            $scope.agreement = {};
+        })
+            
+    }
+
+    var getAgreements = function() {
+        var option = 'get agreements';
+        var idParking = $scope.id;
+        $http.get(urlOptionPrefixAgreement + option + '&idParking=' + idParking).success(function(response) {
+            $scope.agreements = response;
+        })
+
+    }
+    getAgreements();
+	
+}]);
